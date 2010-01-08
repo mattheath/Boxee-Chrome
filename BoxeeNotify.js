8ec02a6d9a62edbe30322eef9720fe60f4a440ff
Gordon Fontenot <gordon.fontenot@gmail.com>
2010-01-08 17:03:09 -0500

    initial commit

---
+++ BoxeeNotify.js
@@ -0,0 +1,34 @@
+findVideos();
+//window.addEventListener("focus", findVideos);
+
+/**
+ * Find all videos on the page. 
+ */
+function findVideos() {
+    var embeds_col = document.getElementsByTagName('embed');
+    var objects = document.getElementsByTagName('object');
+    var count = 0;
+
+    // Convert collection to array
+    var embeds = [];
+    for (var i = 0; i < embeds_col.length; i++) {
+        ++count;
+    }
+
+    // Check object tags that don't have <embed> element
+    for (var i = 0; i < objects.length; i++) {
+        if (!/<embed/.test(objects[i].innerHTML)) {
+            ++count;
+        }
+    }
+
+    if (count > 0)
+    {
+		doNotifyBoxee();
+    }
+};
+
+function doNotifyBoxee() {
+    var port = chrome.extension.connect({name: "boxeeNotify"});
+	port.postMessage({found: (e != undefined)});
+};
