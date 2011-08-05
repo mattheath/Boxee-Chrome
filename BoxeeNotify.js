e4927339851da2e027aea3e8a485a7ff50c1b144
Matt Heath <matt@mattheath.com>
2011-08-05 01:48:36 +0100

    Submit Boxee ID to Watch later queue via XHR

--- BoxeeNotify.js
+++ BoxeeNotify.js
@@ -74,6 +74,8 @@ function addIMDbToQueue(url) {
 
 			var boxeeID = /boxee:id>(\d{1,8})<\/boxee:id/.exec(xhr.responseText);
 			console.log('Response from boxee received id= '+boxeeID[1]);
+
+			submitBoxeeIdToQueue(boxeeID[1])
 		}
 	}
 
@@ -84,4 +86,29 @@ function addIMDbToQueue(url) {
 	xhr.send();
 
 	console.log('Request for boxeeID and data sent');
+}
+
+/**
+ * Submit a Boxee ID to be added to the Watch Later Queue
+ * @todo Currently all media items are submitted as movies, this will likely cause problems
+ */
+function submitBoxeeIdToQueue(boxeeID) {
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(data) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log(xhr.responseText);
+		}
+	}
+
+	var boxeeApiAddUrl = "http://app.boxee.tv/action/add";
+	var params = "<message type=\"queue\"><object type=\"movie\"><activated_from>movies</activated_from><boxee_id>" + boxeeID + "</boxee_id></object></message>";
+	xhr.open("POST", boxeeApiAddUrl, true);
+
+	//Send the proper header information along with the request
+	xhr.setRequestHeader("Content-Type", "text/xml");
+
+	// Submit the POST data
+	xhr.send(params);
+	console.log('XHR request sent to Boxee to add '+boxeeID+' to the watch later queue');
 }
